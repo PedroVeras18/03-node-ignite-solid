7c1cb1fe7e1e4ff3e335f029b77c040c4ca5e176
Pedro Veras <pedro.veras@kingspanisoeste.com.br>
2023-04-27 14:24:50 -0300

    Caso de Uso e Design Patterns: Lidando com erros do use case

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,6 +1,6 @@
 import { hash } from "bcryptjs"
-import { prisma } from "@/lib/prisma"
 import { UserRepository } from "@/repositories/user-repository";
+import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
 
 interface RegisterUseCaseRequest {
     name: string;
@@ -24,7 +24,7 @@ export class RegisterUseCase {
         const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
         if (userWithSameEmail) {
-            throw new Error('E-mail already exists.')
+            throw new UserAlreadyExistsError()
         }
 
         await this.usersRepository.create({
