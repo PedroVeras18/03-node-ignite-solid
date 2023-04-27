b28cc2a09beeee1976fd01beb30004c1c9d466e2
Pedro Veras <pedro.veras@kingspanisoeste.com.br>
2023-04-27 10:07:51 -0300

    Integração com Prisma ORM: Fundamentos do Prisma ORM

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,14 @@
 import fastify from 'fastify';
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+    data: {
+        id: '1',
+        name: 'Diego Fernandes',
+        email: "diego@rocketseat.com.br"
+    }
+})
