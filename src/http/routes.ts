81eb60d31449b526f34260b4ee11f69487da4819
Pedro Veras <pedro.veras@kingspanisoeste.com.br>
2023-04-28 10:43:48 -0300

    Implementando casos de uso: Testes e controller de autenticação

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,9 @@
 import { register } from "./controllers/register";
+import { authenticate } from "./controllers/authenticate";
 import { FastifyInstance } from "fastify";
 
 export async function appRoutes(app: FastifyInstance) {
     app.post('/users', register)
+    
+    app.post('/sessions', authenticate)
 }
