309335249e8149b5edb59d37638765a8d953ef95
Pedro Veras <pedro.veras@kingspanisoeste.com.br>
2023-05-03 11:41:48 -0300

    Controllers & Testes E2E: Implementando JWT no Fastify

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
 import fastify from 'fastify';
+import fastifyJwt from '@fastify/jwt';
+
+import { env } from './env';
 import { ZodError } from 'zod';
 import { appRoutes } from './http/routes';
-import { env } from './env';
 
 export const app = fastify()
+
+app.register(fastifyJwt, {
+    secret: env.JWT_SECRET,
+})
 
 app.register(appRoutes)
 
